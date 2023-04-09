6cf3336a9fdfa70e7ec81bc9bfb017370d329b1c
MynameisMiaoo <sanhsnsg2@gmail.com>
2023-04-09 13:09:11 +0700

    done profile_user

--- app/src/main/java/com/example/myapplication/model/User.java
+++ app/src/main/java/com/example/myapplication/model/User.java
@@ -1,47 +1,63 @@
 package com.example.myapplication.model;
 
 public class User {
+    private int id;
     private String email;
     private String sdt;
     private String dchi;
     private String name;
 
-    public User(String email, String sdt, String dchi, String name) {
-        this.email = email;
-        this.sdt = sdt;
+    public User() {
+    }
+
+    public User(int id,String dchi, String email, String name, String sdt) {
         this.dchi = dchi;
-        this.name = name;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
-    }
-
-    public void setSdt(String sdt) {
-        this.sdt = sdt;
-    }
-
-    public void setDchi(String dchi) {
-        this.dchi = dchi;
-    }
-
-    public void setName(String name) {
         this.name = name;
+        this.sdt = sdt;
+        this.id=id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getSdt() {
         return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
     }
 
     public String getDchi() {
         return dchi;
     }
 
+    public void setDchi(String dchi) {
+        this.dchi = dchi;
+    }
+
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+//    public User GetUserByEmail(String str){
+//        return
+//    }
 }
