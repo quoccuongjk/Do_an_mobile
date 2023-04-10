b9de3c617835ee1f2df61d20768902f2652358b6
KhoaLeTan <lekhoa733@gmail.com>
2023-04-10 21:48:04 +0700

    update

--- app/src/main/java/com/example/myapplication/Profile.java
+++ app/src/main/java/com/example/myapplication/Profile.java
@@ -21,6 +21,7 @@ import com.example.myapplication.fragment.CartFragment;
 import com.example.myapplication.model.Details;
 import com.example.myapplication.model.Food;
 import com.example.myapplication.model.FoodType;
+import com.example.myapplication.model.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -30,20 +31,26 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+
 public class Profile extends AppCompatActivity {
-    Button button_add;
+    int UserId;
+//    Button button_add;
     int count;
-    FirebaseAuth mAuth;
     ImageView imageView1 , imageView2,imageView3, img_food;
     TextView textView1,textView2,textView3,tv_name, tv_mota;
     Food food;
+    String gmail;
+    ArrayList<User> ListUser;
+    User user1;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         count=0;
         setContentView(R.layout.activity_profile);
-        button_add = findViewById(R.id.ok_add);
+//        button_add = findViewById(R.id.ok_add);
         textView3=findViewById(R.id.count_profile);
         textView2 = findViewById(R.id.price_profile);
         textView1 = findViewById(R.id.tong_profile);
@@ -79,7 +86,7 @@ public class Profile extends AppCompatActivity {
                 if(count>0)imageView2.setVisibility(imageView2.VISIBLE);
             }
         });
-            imageView2.setOnClickListener(new View.OnClickListener() {
+        imageView2.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     count--;
@@ -88,36 +95,62 @@ public class Profile extends AppCompatActivity {
                     if(count==0){        imageView2.setVisibility(imageView2.INVISIBLE);
                     }
                 }
-            });
-        button_add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("Details");
-
-                //String gmailUser = (mAuth.getCurrentUser().getEmail());
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                String gmailUser = user.getEmail();
-
-                Log.d("CUONGVIPPRO",gmailUser);
-                int FoodId = food.getId();
-                Details details = new Details("lekhoa734@gmail.com",FoodId,count);
-                myRef.child("4").setValue(details, new DatabaseReference.CompletionListener() {
-                    @Override
-                    public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
-                        Toast.makeText(getApplicationContext(),"Thêm vào giỏ hàng thành công",Toast.LENGTH_SHORT).show();
-                    }
-                });
+        });
+//        button_add.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FirebaseDatabase database = FirebaseDatabase.getInstance();
+//                DatabaseReference myRef = database.getReference("Details");
+//                Log.d("123","123123");
+//                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//                String gmailUser = user.getEmail();
+//                Log.d("Pro",gmailUser);
+//                FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+//                DatabaseReference databaseReference = firebaseDatabase.getReference("User");
+//
+//                databaseReference.addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                        for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+//                            user1 = dataSnapshot.getValue(User.class);
+//                            ListUser.add(user1);
+//                        }
+//                        UserId = GetUser(ListUser,gmail).getId();
+//                    }
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError error) {
+//                        Toast.makeText(getApplicationContext(), "Fail", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//                System.out.print(UserId);
+//                int FoodId = food.getId();
+//                Details details = new Details(UserId,FoodId,count);
+//                String child = String.valueOf(UserId);
+//                myRef.child(child).setValue(details, new DatabaseReference.CompletionListener() {
+//                    @Override
+//                    public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+//                        Toast.makeText(getApplicationContext(),"Thêm vào giỏ hàng thành công",Toast.LENGTH_SHORT).show();
+//                    }
+//                });
 //                Intent intent = new Intent(getApplicationContext(), CartFragment.class);
 //                startActivity(intent);
-
-            }
-        });
+//
+//            }
+//        });
     }
 
 
 
     private int TongTien() {
         return Integer.parseInt(textView2.getText().toString())*count;
+    }
+    public User GetUser(ArrayList<User> arrayList, String s){
+        for (User user : arrayList) {
+            if (user.getEmail().trim().equals(gmail.trim()))
+            {
+                return user;
+            }
+        }
+        return new User(0,"Edit Pls!",gmail,"Your Name","Edit Pls!");
     }
 }
