11c628fc3acf57ebcfec6779f4da615425f46c6d
quoccuongjk <ngoquoccuongfml1@gmail.com>
2023-04-10 13:54:34 +0700

    ok khoa

--- app/src/main/java/com/example/myapplication/model/Details.java
+++ app/src/main/java/com/example/myapplication/model/Details.java
@@ -1,22 +1,22 @@
 package com.example.myapplication.model;
 
 public class Details {
-    private int Id_User ;
+    private String Gmail_User ;
     private  int Id_Food;
     private int Cunt_Food;
 
-    public Details(int id_User, int id_Food, int cunt_Food) {
-        Id_User = id_User;
+    public Details(String gmail_User, int id_Food, int cunt_Food) {
+        Gmail_User = gmail_User;
         Id_Food = id_Food;
         Cunt_Food = cunt_Food;
     }
 
-    public int getId_User() {
-        return Id_User;
+    public String getGmail_User() {
+        return Gmail_User;
     }
 
-    public void setId_User(int id_User) {
-        Id_User = id_User;
+    public void setGmail_User(String gmail_User) {
+        Gmail_User = gmail_User;
     }
 
     public int getId_Food() {
